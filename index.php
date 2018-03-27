c175e7bb8638eeb6903f086f7ace334d69da6c06
Adrian Amaglio <Vous@exemple.com>
2018-03-27 09:05:47 +0200

    faq

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <?php
+/*
+ * Page displays in french because it will be mostly used by french. Feel free to add language system.
+ * TODO Cache the humongous API data
+*/
+
+/* Fetching velov API in an associative array. This form is easier to use as keys are ids */
 $data =
-json_decode(
-file_get_contents(
-  'https://velov.grandlyon.com/fr/les-stations.html?type=777&tx_glstationsvelov_pi1%5Baction%5D=listOfVelovWidthInfoStation&tx_glstationsvelov_pi1%5Bcontroller%5D=StationVelov',
-  false,
-  stream_context_create([
-    'http' => [
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'POST',
-        'content' => http_build_query([])
-      ]
-    ])
-  ),
-  true
-);
+  json_decode(
+    file_get_contents(
+      'https://velov.grandlyon.com/fr/les-stations.html?type=777&tx_glstationsvelov_pi1%5Baction%5D=listOfVelovWidthInfoStation&tx_glstationsvelov_pi1%5Bcontroller%5D=StationVelov',
+      false,
+      stream_context_create([
+        'http' => [
+          'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+          'method'  => 'POST',
+          'content' => http_build_query([])
+        ]
+      ])
+    ),
+    true
+  );
+
+/* Check if a filter is enabled */
 
 if(isset($_GET['filter'])){
   if(htmlentities($_GET['filter']))
@@ -75,5 +83,13 @@ if(isset($_GET['filter'])){
     <input type="submit" value="Afficher uniquement la séléction" />
   <?php } ?>
 </form>
+<p>
+  <h4>À quoi ça sert ?</h4>
+  À garder ses stations habituelles (et leurs voisines) dans un favoris de navigateur ! Plus besoin de se ballader sur une carte tous les jours ! Cet outil est également accessible sur tous les téléphones peu performants, contrairement au site officiel.
+</p>
+<p>
+  <h4>Pourquoi cette page est elle lente ?</h4>
+  Parce qu’il faut télécharger toutes les données relatives aux stations vélov à chaque chargement ! N’hésitez pas à améliorer cette page sur <a href="http://github.com/adrianamaglio/velov">Github</a>
+</p>
 </body>
 </html>
